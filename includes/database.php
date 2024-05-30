fc8f537a078bf56a6a549af23ed13b8b02080a97
LoydNgei <loydngei@gmail.com>
2024-05-30 18:37:29 +0300

    FIle connecting to Database

---
+++ includes/database.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'cms';
+
+$connect = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if (mysqli_connect_errno()) {
+    exit("Failed to connect to MySQL: " . mysqli_connect_error());
+}
