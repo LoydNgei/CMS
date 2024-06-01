f9151e196447f03b2e896bd9ef1d05a7cef8405f
LoydNgei <loydngei@gmail.com>
2024-06-01 22:53:14 +0300

    Created functions file to handle my functions

---
+++ includes/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function secure() {
+    if(!isset($_SESSION['id'])){
+        set_message("Please Login to view page");
+        header("Location: /");
+        die();
+    }
+}
+
+
+function set_message($message) {
+    {
+        $_SESSION['message'] = $message;
+    }
+}
+
+function get_message() {
+    if(isset($_SESSION['message'])) {
+        echo '<p>' . $_SESSION['message'] . '</p> <hr>';
+        unset($_SESSION['message']);
+    }
+}
